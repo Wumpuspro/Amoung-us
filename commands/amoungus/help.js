53ed44972dcedd350d0cce7bba843e60628d1085
Wumpus 1942 <86592522+Wumpuspro@users.noreply.github.com>
2021-08-11 21:16:43 +0545

    Create help.js

---
+++ commands/amoungus/help.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name : 'help',
+    run : async(client, message, args) => {
+        message.channel.send('Click here for more information on how to use the bot -> https://github.com/Wumpuspro/Amoung-us//blob/main/README.md#-commands')
+    }
+}
